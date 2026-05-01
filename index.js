14804d49636b676d1ee295fc2b796c46a032608e
thpasorroui-sketch <thpa.sorro.ui@phinmaed.com>
2026-05-01 20:20:09 +0800

    Changeees

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ Orders.forEach(order => {
                         <td>${order.productNumber}</td>
                         <td>${order.paymentStatus}</td>
                         <td class="${
-                             order.shipping === 'Declined' ? 'danger' :
+                            order.shipping === 'Declined' ? 'danger' :
                             order.shipping === 'Pending' ? 'warning' : 'primary'
                         }">${order.shipping}</td>
                         <td class="primary">Details</td>
